32c31d4724c3225ebe211550bd2a5ee202b7db71
Awesome Quest <torfit@gmail.com>
2024-12-11 11:19:04 +0000

    final d8

--- torfi/skil5/animateD8.m
+++ torfi/skil5/animateD8.m
@@ -1,18 +1,18 @@
 clc;close all;
-T = 100;
+T = 200;
 
-N = 100;
-M = 100;
+N = 1000;
+M = N;
 
 
-% [us1,ts,xs1] = iterdiffd8(T,N,M);
-[us1,ts,xs1] = iterdiffd8org(T,N,M);
+[us1,ts,xs1] = iterdiffd8(T,N,M);
+% [us1,ts,xs1] = iterdiffd8org(T,N,M);
 
 % plot(xs1,us1(:,round(N/6*6)))
 
-for i = 1:N+1
+for i = 1:10:N+1
     plot(xs1,us1(:,i))
-    text(2.5, mean(us1(:,i)),num2str(ts(i)))
+    text(2.5,2.5,num2str(ts(i)))
     ylim([0,1])
     pause(0.1)
 end
--- torfi/skil5/iterdiffd8.m
+++ torfi/skil5/iterdiffd8.m
@@ -1,48 +1,45 @@
 function [us,ts,xs] = iterdiffd8(T,N,M)
-    D = 0.01; v = 0.1; L = 5; h = L/M; k = T/N;
-    alpha = k*D/h^2; beta  = v*k/2/h;
-    ts = 0:k:T; xs = 0:h:L;
+D = 0.01; v = 0.1; L = 5; h = L/M; k = T/N;
+alpha = k*D/h^2; beta = v*k/2/h;
+ts = 0:k:T; xs = 0:h:L;
 
-    A = sparse(M+1,M+1);
-    A(1,1) = 1; A(end,end) = 3; A(end,end-1) = -4; A(end,end-2) = 1;
-    for i = 2:M
-        A(i,i-1) = -beta-alpha;
-        A(i,i) = 1+2*alpha;
-        A(i,i+1) = beta-alpha;
-    end
+A = sparse( ...
+    [2:M, 2:M, 2:M],[1:M-1, 2:M, 3:M+1], ...
+    [repelem(-beta-alpha,M-1), ...
+        repelem(1+2*alpha,  M-1), ...
+        repelem(beta-alpha, M-1), ...
+    ],M+1,M+1 ...
+);
+A(1,1) = 1; A(end,end) = 3; A(end,end-1) = -4; A(end,end-2) = 1;
 
-    A2 = sparse(M+1,M+1);
-    A2(1,1) = 1; A2(end,end) = 3; A2(end,end-1) = -4; A2(end,end-2) = 1;
-    for i = 2:M
-        A2(i,i-1) = -2*beta-2*alpha;
-        A2(i,i) = 3+4*alpha;
-        A2(i,i+1) = 2*beta-2*alpha;
-    end
+A2 = sparse( ...
+    [2:M, 2:M, 2:M],[1:M-1, 2:M, 3:M+1],...
+    [repelem(-2*beta-2*alpha,M-1), ...
+        repelem(3+4*alpha,      M-1), ...
+        repelem(2*beta-2*alpha, M-1), ...
+    ], M+1,M+1 ...
+);
+A2(1,1) = 1; A2(end,end) = 3; A2(end,end-1) = -4; A2(end,end-2) = 1;
 
-    us = zeros(M+1,N+1);
-    us(:,2) = A\us(:,1);
+us = zeros(M+1,N+1);
+bound = [
+    ((0:(20/T*N)))*k/20, ...
+    ones(1,30/T*N- 20/T*N), ...
+    2.5 - ((30/T*N:50/T*N-1))*k/20, ...
+    zeros(1,(T-50)/T*N)
+];
+us(1,:) = bound;
+us(:,2) = A\us(:,1);
+us(1,2) = bound(2);
 
-    bv = us(:,2);
-    bv(1) = 1*k/20; bv(end) = 0;
-    bv(2:M)  = 4*us(2:M,2) - us(2:M,1);
-    counts = [];
-    for j = 3:N+1
-        us(:,j) = A2\bv;
-        if j <= 20/T*N
-            bv(1) = (j-1)*k/20;
-            counts(1) = j;
-        elseif j >= 20/T*N && j <= 30/T*N
-            bv(1) = 1;
-            counts(2) = j;
-        elseif j >= 30/T*N && j <= 50/T*N
-            bv(1) = 2.5 - (j-1)*k/20;
-            counts(3) = j;
-        else
-            bv(1) = 0;
-            counts(4) = j;
-        end
-        bv(2:M+1)  = 4*us(2:M+1,j) - us(2:M+1,j-1);
-        bv(end) = 0;
-    end
-    disp(counts * k)
+bv = us(:,2);
+bv(end) = 0;
+bv(2:M)  = 4*us(2:M,2) - us(2:M,1);
+for j = 3:N+1
+    us(:,j) = A2\bv;
+    us(1,j) = bound(j);
+    bv(1) = bound(j); bv(end) = 0;
+    bv(2:M) = 4*us(2:M,j) - us(2:M,j-1);
+end
+disp([us(1,:)' bound'])
 end
